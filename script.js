0c4f8f69172302805a332027c88337442b442bd0
John Gibson <jhngbsn09@gmail.com>
2022-09-24 14:29:51 -0400

    Changed fuction do delete zero at start

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 let display = document.getElementById('input');
 
 function displayChange(inp){
-    display.innerHTML += inp;
+    if (display.innerHTML == 0){
+        display.innerHTML = inp;
+    } else {
+        display.innerHTML += inp
+    }
+    
 }
